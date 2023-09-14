2ad3e48772a7407fc694353678abff513499a295
Ricardo Dirani <dirani@gmail.com>
2023-09-14 06:12:21 -0300

    Create CustomerController and CustomerService

--- src/main/java/guru/springframework/spring6restmvc/controller/CustomerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/CustomerController.java
@@ -1,4 +1,14 @@
 package guru.springframework.spring6restmvc.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("api/v1/customer")
 public class CustomerController {
+
 }
