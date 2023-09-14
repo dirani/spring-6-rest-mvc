215b9322fd1b256a0ac40d3b5f85c521191d5424
Ricardo Dirani <dirani@gmail.com>
2023-09-14 09:47:14 -0300

    Update CustomerServiceImpl.java

--- src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
@@ -1,4 +1,28 @@
 package guru.springframework.spring6restmvc.services;
 
+import guru.springframework.spring6restmvc.model.Beer;
+import guru.springframework.spring6restmvc.model.Customer;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class CustomerServiceImpl {
+    private Map<UUID, Customer> customerMap;
+
+    public CustomerServiceImpl(){
+        this.customerMap = new HashMap<>();
+    }
+
 }
+
+/*
+public class Customer {
+    private String customerName;
+    private UUID id;
+    private Integer version;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+
+}*/
