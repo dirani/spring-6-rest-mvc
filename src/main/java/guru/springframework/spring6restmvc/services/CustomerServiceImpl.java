64f6893051a6a6afcd82266b562f89069449468b
Ricardo Dirani <dirani@gmail.com>
2023-09-14 10:39:52 -0300

    Update CustomerServiceImpl.java

--- src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
@@ -1,28 +1,63 @@
 package guru.springframework.spring6restmvc.services;
 
 import guru.springframework.spring6restmvc.model.Beer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
 import guru.springframework.spring6restmvc.model.Customer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
-public class CustomerServiceImpl {
+@Slf4j
+@Service
+public class CustomerServiceImpl implements CustomerService {
     private Map<UUID, Customer> customerMap;
 
-    public CustomerServiceImpl(){
+    public CustomerServiceImpl() {
         this.customerMap = new HashMap<>();
     }
 
-}
+    Customer customer1 = Customer.builder()
+            .customerName("Eric Cavalier")
+            .id(UUID.randomUUID())
+            .version(1)
+            .createdDate(LocalDateTime.now())
+            .lastModifiedDate(LocalDateTime.now())
+            .build();
+
+
+    customerMap.put(customer1.getId(), customer1);
+
+    @Override
+    public List<Customer> ListCustomers() {
+        return new ArrayList<>(customerMap.values());
+    }
+
+    @Override
+    public Customer getCustomerById(UUID id) {
+        log.debug("Get Customer by Id - in service. Id: " + id.toString());
+
+        return customerMap.get(id);
+    }
+
 
 /*
-public class Customer {
-    private String customerName;
-    private UUID id;
-    private Integer version;
-    private LocalDateTime createdDate;
-    private LocalDateTime lastModifiedDate;
+        beerMap.put(beer1.getId(), beer1);
+        beerMap.put(beer2.getId(), beer2);
+        beerMap.put(beer3.getId(), beer3);
+}
 
-}*/
+    @Override
+    public List<Beer> listBeers(){
+        return new ArrayList<>(beerMap.values());
+    }
+
+    @Override
+    public Beer getBeerById(UUID id) {
+
+        log.debug("Get Beer by Id - in service. Id: " + id.toString());
+
+        return beerMap.get(id);*/
+}
