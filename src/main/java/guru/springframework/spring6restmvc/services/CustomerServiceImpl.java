f0a253bb94fc61a631e8f2d5e76bfbb19b8ab399
Ricardo Dirani <dirani@gmail.com>
2023-09-14 16:03:52 -0300

    CustomerController & CustomerServiceImpl

--- src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
@@ -17,47 +17,29 @@ public class CustomerServiceImpl implements CustomerService {
 
     public CustomerServiceImpl() {
         this.customerMap = new HashMap<>();
+
+        Customer customer1 = Customer.builder()
+                .customerName("Eric Cavalier")
+                .id(UUID.randomUUID())
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .build();
+
+        customerMap.put(customer1.getId(), customer1);
     }
 
-    Customer customer1 = Customer.builder()
-            .customerName("Eric Cavalier")
-            .id(UUID.randomUUID())
-            .version(1)
-            .createdDate(LocalDateTime.now())
-            .lastModifiedDate(LocalDateTime.now())
-            .build();
 
-
-    // customerMap.put(customer1.getId(), customer1);
 
     @Override
-    public List<Customer> ListCustomers() {
-        return new ArrayList<>(customerMap.values());
-    }
+    public List<Customer> ListCustomers() {return new ArrayList<>(customerMap.values());}
 
     @Override
     public Customer getCustomerById(UUID id) {
+
         log.debug("Get Customer by Id - in service. Id: " + id.toString());
 
         return customerMap.get(id);
     }
 
-
-/*
-        beerMap.put(beer1.getId(), beer1);
-        beerMap.put(beer2.getId(), beer2);
-        beerMap.put(beer3.getId(), beer3);
-}
-
-    @Override
-    public List<Beer> listBeers(){
-        return new ArrayList<>(beerMap.values());
-    }
-
-    @Override
-    public Beer getBeerById(UUID id) {
-
-        log.debug("Get Beer by Id - in service. Id: " + id.toString());
-
-        return beerMap.get(id);*/
 }
